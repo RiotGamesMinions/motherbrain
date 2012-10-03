e836ad4e66230aec238ed4c7f8ba91c277a9ccba
Jamie Winsor <jamie@vialstudios.com>
2012-10-03 03:00:11 -0700

    add documentation for MB::Gear

--- lib/mb/gear.rb
+++ lib/mb/gear.rb
@@ -1,26 +1,69 @@
 module MotherBrain
   # @author Jamie Winsor <jamie@vialstudios.com>
+  #
+  # The base module for defining new {Gears} for a plugin. Any class including this module
+  # will be registered with {MotherBrain} as a {Gear}.
+  #
+  # Gears represent the proverbial knobs and levers that can be used to manipulate a
+  # {Component}.
+  #
+  # @example Defining a new Gear
+  #
+  #   class Twitter
+  #     include MotherBrain::Gear
+  #   end
+  #
+  # @example Plugin DSL usage
+  #
+  #   component do
+  #     name "news_post"
+  #
+  #     twitter do
+  #     ...
+  #     end
+  #   end
+  #
   module Gear
     class << self
+      # Registers a given Class as a Gear to be used within MotherBrain plugins. This
+      # function is automatically called when {MotherBrain::Gear} is included into
+      # a Class. You probably don't want to call this function directly.
+      #
+      # @api private
+      #
       # @param [~MotherBrain::Gear] klass
       def register(klass)
         all.add(klass)
       end
 
+      # Return a Set containing all of the registered Gears that can be used within
+      # a MotherBrain plugin.
+      #
       # @return [Set<MotherBrain::Gear>]
       def all
         @all ||= Set.new
       end
 
+      # Clears all of the registered Gears and then traverses the ObjectSpace to find
+      # all classes which include {MotherBrain::Gear} and calls {#register} with each.
+      #
+      # @return [Set<MotherBrain::Gear>]
+      #   the Set of registered Gears
       def reload!
+        clear!
         ObjectSpace.each_object(::Module).each do |mod|
           if mod < MB::Gear
             register(mod)
           end
         end
+
+        all
       end
 
+      # Clears all of the registered Gears.
+      #
       # @return [Set]
+      #   an empty Set
       def clear!
         @all = Set.new
       end
@@ -31,22 +74,44 @@ module MotherBrain
     included do
       class_eval do
         set_keyword(self.to_s.demodulize.underscore)
-        register_gear
+        Gear.register(self)
         include RealObject
       end
     end
 
     module ClassMethods
+      # The identifier for the Gear. The keyword is automatically populated based on the name
+      # of the Class including {MotherBrain::Gear}. The keyword must be unique among the other
+      # registered Gears. Also used to define a Gear in the plugin DSL.
+      #
       # @return [Symbol]
       attr_reader :keyword
 
+      # Explicitly set the keyword for a Gear. This is useful if you have a class whose name
+      # conflicts with an already registered Gear or if you have a long class name that you want
+      # simplified.
+      #
+      # @example setting a non default keyword
+      #
+      #     class TwitterFeed
+      #       include MotherBrain::Gear
+      #
+      #       set_keyword :twitter
+      #     end
+      #
+      # @example gear can now be identified to the DSL by 'twitter' instead of 'twitter_feed'
+      #
+      #   component do
+      #     name "news_post"
+      #
+      #      twitter do
+      #      ...
+      #      end
+      #    end
+      # 
       # @param [#to_sym] value
       def set_keyword(value)
         @keyword = value.to_sym
-      end
-
-      def register_gear
-        Gear.register(self)
       end
     end
   end
