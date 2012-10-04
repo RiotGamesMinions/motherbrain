389001f28f95a8441496079a3c29f46c5c9c73db
Jamie Winsor <jamie@vialstudios.com>
2012-10-04 03:17:19 -0700

    function documentation

--- lib/motherbrain.rb
+++ lib/motherbrain.rb
@@ -15,31 +15,35 @@ require 'mb/errors'
 
 # @author Jamie Winsor <jamie@vialstudios.com>
 module MotherBrain
-  autoload :Mixin, 'mb/mixin'
+  autoload :Command, 'mb/command'
+  autoload :CommandRunner, 'mb/command_runner'
+  autoload :Component, 'mb/component'
+  autoload :ComponentInvoker, 'mb/component_invoker'
   autoload :Config, 'mb/config'
+  autoload :Context, 'mb/context'
+  autoload :DynamicGears, 'mb/dynamic_gears'
+  autoload :DynamicInvoker, 'mb/dynamic_invoker'
+  autoload :Gear, 'mb/gear'
+  autoload :Group, 'mb/group'
   autoload :InvokerBase, 'mb/invoker_base'
   autoload :Invoker, 'mb/invoker'
-  autoload :DynamicInvoker, 'mb/dynamic_invoker'
-  autoload :DynamicGears, 'mb/dynamic_gears'
+  autoload :Mixin, 'mb/mixin'
   autoload :Plugin, 'mb/plugin'
   autoload :PluginDSL, 'mb/plugin_dsl'
   autoload :PluginInvoker, 'mb/plugin_invoker'
-  autoload :PluginLoader, 'mb/plugin_loader'
-  autoload :Component, 'mb/component'
-  autoload :ComponentInvoker, 'mb/component_invoker'
-  autoload :Command, 'mb/command'
-  autoload :CommandRunner, 'mb/command_runner'
-  autoload :Group, 'mb/group'
+  autoload :PluginLoader, 'mb/plugin_loader'  
   autoload :ProxyObject, 'mb/proxy_object'
-  autoload :RealObject, 'mb/real_object'
-  autoload :Gear, 'mb/gear'
-  autoload :Context, 'mb/context'
+  autoload :RealObject, 'mb/real_object'  
 
   class << self
+    attr_writer :ui
+    
+    # @return [Thor::Shell::Color]
     def ui
       @ui ||= Thor::Shell::Color.new
     end
 
+    # @return [Pathname]
     def root
       @root ||= Pathname.new(File.expand_path('../', File.dirname(__FILE__)))
     end
