b0c9ad487006fe9e5f039a26363449a4f775d724
jhowarth <jhowarth@riotgames.com>
2012-10-16 12:19:24 -0700

    Add test for multiple groups on on().

--- spec/unit/mb/command_spec.rb
+++ spec/unit/mb/command_spec.rb
@@ -42,31 +42,51 @@ describe MB::Command::CommandRunner do
 
   let(:node_1) { double('node_1', name: 'reset.riotgames.com') }
   let(:node_2) { double('node_2', name: 'jwinsor.riotgames.com') }
-  let(:nodes) { [ node_1, node_2 ] }
+  let(:node_3) { double('node_3', name: 'jwinsor.riotgames.com') }
+  let(:nodes) { [ node_1, node_2, node_3 ] }
 
-  let(:group) { double('group', nodes: nodes) }
+  let(:master_group) { double('master_group', nodes: [ node_1, node_2 ]) }
+  let(:slave_group) { double('slave_group', nodes: [ node_3 ]) }
 
   subject { MB::Command::CommandRunner }
 
   describe "#on" do
+
     before(:each) do
-      @proc = Proc.new do
-        on("group") do
+      MB::Command::CommandRunner::CleanRoom.stub_chain(:new, :actions).and_return(actions)
+    end
+
+    it "has a single group" do
+      scope.should_receive(:group!).with("master_group").and_return(master_group)
+
+      actions.each do |action|
+        action.should_receive(:run).with(master_group.nodes)
+      end
+
+      command_block = Proc.new do
+        on("master_group") do
           # block
         end
       end
+      
+      subject.new(@context, scope, command_block)
+    end
 
-      MB::Command::CommandRunner::CleanRoom.stub_chain(:new, :actions).and_return(actions)
+    it "has multiple groups" do
+      scope.should_receive(:group!).with("master_group").and_return(master_group)
+      scope.should_receive(:group!).with("slave_group").and_return(slave_group)
 
       actions.each do |action|
         action.should_receive(:run).with(nodes)
       end
 
-      scope.stub(:group!).and_return(group)
-    end
-
-    it "initializes" do
-      subject.new(@context, scope, @proc)
+      command_block = Proc.new do
+        on(["master_group", "slave_group"]) do
+          # block
+        end
+      end
+      
+      subject.new(@context, scope, command_block)
     end
   end
 end
