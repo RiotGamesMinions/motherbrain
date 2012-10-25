70a7e15871967b8b83341adc0725a4af988e0921
jhowarth <jhowarth@riotgames.com>
2012-10-25 15:11:38 -0700

    Allow users to use MB without JRuby.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,30 +1,25 @@
 require 'rubygems'
 require 'bundler'
-require 'spork'
 
-Spork.prefork do
-  require 'rspec'
-  require 'aruba/cucumber'
+require 'rspec'
+require 'aruba/cucumber'
 
-  Dir[File.join(File.expand_path("../../../spec/support/**/*.rb", __FILE__))].each { |f| require f }
+Dir[File.join(File.expand_path("../../../spec/support/**/*.rb", __FILE__))].each { |f| require f }
 
-  RSpec.configure do |config|
-    config.include MotherBrain::SpecHelpers
+RSpec.configure do |config|
+  config.include MotherBrain::SpecHelpers
 
-    config.before(:each) do
-      clean_tmp_path
-    end
-  end
-
-  World(Aruba::Api)
-  World(MotherBrain::SpecHelpers)
-  
-  Before do
-    set_mb_config_path
-    set_plugin_path
+  config.before(:each) do
+    clean_tmp_path
   end
 end
 
-Spork.each_run do  
-  require 'motherbrain'
+World(Aruba::Api)
+World(MotherBrain::SpecHelpers)
+
+Before do
+  set_mb_config_path
+  set_plugin_path
 end
+
+require 'motherbrain'
