89729bc15a49c313e7eef6708c5a403e187af389
Justin Campbell <justin@justincampbell.me>
2012-11-08 18:28:42 -0500

    Mutexes are per-environment

--- lib/mb/command.rb
+++ lib/mb/command.rb
@@ -32,7 +32,7 @@ module MotherBrain
 
     # Run the proc stored in execute with the given arguments
     def invoke(*args)
-      ChefMutex.new(name, context.chef_conn).synchronize do
+      ChefMutex.new(environment, context.chef_conn).synchronize do
         CommandRunner.new(context, scope, execute)
       end
     end
