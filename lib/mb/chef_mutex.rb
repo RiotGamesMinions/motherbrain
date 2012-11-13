3e0a89715a22f382d6f6eb66042737df28e2209f
Justin Campbell <justin@justincampbell.me>
2012-11-13 12:03:36 -0500

    Remove extraneous single quote

--- lib/mb/chef_mutex.rb
+++ lib/mb/chef_mutex.rb
@@ -70,7 +70,7 @@ module MotherBrain
           current_lock = read
 
           raise ResourceLocked,
-            "Resource '#{current_lock['id']} locked by #{current_lock['client_name']} since #{current_lock['time']}\n" # +
+            "Resource #{current_lock['id']} locked by #{current_lock['client_name']} since #{current_lock['time']}\n" # +
             # "(Try again in a few moments, or use --force/-f to override)"
         end
 
