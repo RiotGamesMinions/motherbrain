e989173e33d05373c72a9f106de56f8c1205262e
Jamie Winsor <jamie@vialstudios.com>
2012-11-16 01:32:35 -0800

    add initial tests for manifest

--- lib/mb/provisioner/manifest.rb
+++ lib/mb/provisioner/manifest.rb
@@ -3,34 +3,51 @@ module MotherBrain
     # @author Jamie Winsor <jamie@vialstudios.com>
     class Manifest
       class << self
+        # @param [#to_s] path
+        #
+        # @return [Provisioner::Manifest]
         def from_file(path)
-          path = File.expand_path(path)
+          path = File.expand_path(path.to_s)
           data = File.read(path)
           new(path).from_json(data)
         end
 
+        # Validate a Hash representing a provisioner manifest
+        #
+        # @param [Hash] manifest_hash
+        #
+        # @raise [InvalidProvisionManifest] if the given Hash is not well formed
+        #
+        # @return [Boolean]
         def validate(manifest_hash)
+          unless manifest_hash.is_a?(Hash)
+            raise InvalidProvisionManifest, "Provisioner manifest must be a Hash"
+          end
+
           true
         end
       end
 
+      # return [String]
       attr_reader :path
+
+      # @return [Hash]
       attr_reader :attributes
 
       # @param [#to_s] path
       def initialize(path = nil)
-        @path = path.to_s
+        @path       = path.to_s
         @attributes = Hash.new
       end
 
       def save
-        if self.path.nil?
+        unless self.path.present?
           raise InternalError, "Cannot save manifest without a destination. Set the 'path' attribute on your object."
         end
 
         FileUtils.mkdir_p(File.dirname(self.path))
         File.open(self.path, 'w+') do |f|
-          f.write(self.to_json(pretty: true))
+          f.write(MultiJson.dump(self.attributes, pretty: true))
         end
 
         self
--- spec/unit/mb/provisioner/manifest_spec.rb
+++ spec/unit/mb/provisioner/manifest_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe MB::Provisioner::Manifest do
-  pending
+  describe "ClassMethods" do
+    subject { described_class }
+
+    describe "::validate" do
+      it "returns true given a hash in the proper format" do
+        subject.validate(Hash.new).should be_true
+      end
+
+      it "raises InvalidProvisionManifest if given a non-hash value" do
+        expect {
+          subject.validate(1)
+        }.to raise_error(MB::InvalidProvisionManifest)
+      end
+    end
+
+    describe "::from_file" do
+      pending
+    end
+  end
+
+  describe "#path" do
+    it { subject.path.should be_a(String) }
+  end
 end
