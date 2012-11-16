d5da0a80addd6b0bbe5f9630ef04bdf23d6bacf4
Justin Campbell <justin@justincampbell.me>
2012-11-16 16:14:41 -0500

    Document ErrorHandler

--- lib/mb/error_handler.rb
+++ lib/mb/error_handler.rb
@@ -1,10 +1,57 @@
 module MotherBrain
   # @author Justin Campbell <justin@justincampbell.me>
+  #
+  # An object to help with the display of errors in a more user-friendly format.
+  # An ErrorHandler is created with an error and a set of options to control the
+  # display of the error. Some options can be inferred from the error itself.
+  # A typical use case would be to wrap an error generated deep in a call stack,
+  # and then add data to the error as it bubbles up.
+  #
+  # @example Wrapping and raising an error with more data
+  #
+  #   ErrorHandler.wrap StandardError.new,
+  #     file_path: "/a/b/c.rb",
+  #     method_name: :wat,
+  #     plugin_name: "hi",
+  #     plugin_version: "1.2.3",
+  #     text: "Invalid thing"
+  #
+  #   # Would raise an error with a message of:
+  #
+  #   hi (1.2.3)
+  #   /a/b/c.rb, on line 1, in 'wat'
+  #   Invalid thing
+  #
+  # @example Wrapping an error at multiple points in the call chain
+  #
+  #   def load_file(path)
+  #     load File.read(path)
+  #   rescue => error
+  #     ErrorHandler.wrap error, file_path: path
+  #   end
+  #
+  #   def load(code)
+  #     eval code
+  #   rescue => error
+  #     ErrorHandler.wrap error, plugin_name: code.lines.to_a.first
+  #   end
+  #
+  #   def method_missing(method_name, *args, &block)
+  #     ErrorHandler.wrap CodeError.new, method_name: method_name
+  #   end
+  #
+  #
   class ErrorHandler
-    def self.wrap(error, options = {})
-      error_handler = new error, options
+    class << self
+      # Wraps an error with additional data and raises it.
+      #
+      # @raise [StandardError]
+      # @see #initialize
+      def wrap(error, options = {})
+        error_handler = new error, options
 
-      raise error_handler.error, error_handler.message
+        raise error_handler.error, error_handler.message
+      end
     end
 
     attr_reader :error
@@ -22,6 +69,27 @@ module MotherBrain
       attr_reader option
     end
 
+    # @param [StandardError] error
+    #
+    # @option options [Array] caller_array
+    #   An array of strings containing filenames, line numbers, and method
+    #   names. Typically comes from `caller`.
+    #
+    # @option options [String] file_path
+    #   The location of a file on disk to display to the user.
+    #
+    # @option options [Symbol] method_name
+    #   The name of the method or keyword which generated the error.
+    #
+    # @option options [String] plugin_name
+    #   The name of the plugin the error relates to.
+    #
+    # @option options [String] plugin_version
+    #   The version of the plugin the error relates to.
+    #
+    # @option options [String] text
+    #   A custom error message to display to the user.
+    #
     def initialize(error, options = {})
       @error = error
 
@@ -31,14 +99,19 @@ module MotherBrain
       embed_data_in_error
     end
 
+    # Extracts data from an options hash and stores it in instance variables.
+    #
+    # @param [Hash] options
+    #
+    # @see #initialize
     def extract_data_from_options(options)
       OPTIONS.each do |option|
-        data = options[option]
-
-        instance_variable_set "@#{option}", data
+        instance_variable_set "@#{option}", options[option]
       end
     end
 
+    # Extracts data from the error and stores it in instance variables. Does not
+    # overwrite existing instance variables.
     def extract_data_from_error
       OPTIONS.each do |option|
         data = error.send option if error.respond_to? option
@@ -49,18 +122,11 @@ module MotherBrain
       end
 
       @caller_array ||= error.backtrace
-      @method_name || error.name if error.respond_to? :name
+      @method_name ||= error.name if error.respond_to? :name
       @text ||= error.message
     end
 
-    def message
-      [
-        plugin_name_and_plugin_version,
-        file_path_and_line_number_and_method_name,
-        text
-      ].compact.join "\n"
-    end
-
+    # Stores the data in the error and defines getters.
     def embed_data_in_error
       OPTIONS.each do |option|
         data = instance_variable_get "@#{option}"
@@ -72,6 +138,16 @@ module MotherBrain
       end
     end
 
+    # @return [String]
+    def message
+      [
+        plugin_name_and_plugin_version,
+        file_path_and_line_number_and_method_name,
+        text
+      ].compact.join "\n"
+    end
+
+    # @return [String]
     def file_path_and_line_number_and_method_name
       buffer = []
       buffer << file_path if file_path
@@ -80,16 +156,16 @@ module MotherBrain
       buffer.join ", "
     end
 
-    def line_number
-      if caller_array
-        caller_array[0].split(":")[1].to_i
-      end
+    # @return [String]
+    def plugin_name_and_plugin_version
+      "#{plugin_name} (#{plugin_version})" if plugin_name and plugin_version
     end
 
-    def plugin_name_and_plugin_version
-      if plugin_name and plugin_version
-        "#{plugin_name} (#{plugin_version})"
-      end
+    # Extracts the first line number from caller_array.
+    #
+    # @return [Fixnum]
+    def line_number
+      caller_array[0].split(":")[1].to_i if caller_array
     end
   end
 end
