99b28c0c52159a66ffe03cce8bb9593009068072
Justin Campbell <justin@justincampbell.me>
2012-11-28 18:55:24 -0500

    Properly format messages starting with a node

--- lib/mb/logging/basic_format.rb
+++ lib/mb/logging/basic_format.rb
@@ -2,7 +2,11 @@ module MotherBrain
   module Logging
     class BasicFormat < Logger::Formatter
       def call(severity, datetime, progname, msg)
-        "[#{datetime.utc.iso8601}] PID[#{Process.pid}] TID[#{Thread.current.object_id.to_s(36)}] #{severity}: #{msg}\n"
+        if match = msg.match(/NODE\[.+?\]/)
+          "#{msg.lines.to_a.map { |line| line.start_with?(match.to_s) ? line : "#{match} #{line}" }.join}"
+        else
+          "[#{datetime.utc.iso8601}] PID[#{Process.pid}] TID[#{Thread.current.object_id.to_s(36)}] #{severity}: #{msg}\n"
+        end
       end
     end
   end
