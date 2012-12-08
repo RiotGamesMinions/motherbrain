d6fad69bad43a9d799071964285ecf87a4e66879
Jamie Winsor <jamie@vialstudios.com>
2012-12-08 15:24:50 -0800

    raise a descriptive error if attempt to call an unregistered actor

--- lib/mb/application.rb
+++ lib/mb/application.rb
@@ -50,24 +50,28 @@ module MotherBrain
 
       # @return [Celluloid::Actor(ConfigSrv)]
       def config_srv
-        Celluloid::Actor[:config_srv]
+        Celluloid::Actor[:config_srv] or raise DeadActorError, "config srv not running"
       end
 
       # @return [Celluloid::Actor(Provisioner::Manager)]
       def provisioner_manager
-        Celluloid::Actor[:provisioner_manager]
+        Celluloid::Actor[:provisioner_manager] or raise DeadActorError, "provisioner manager not running"
       end
       alias_method :provisioner, :provisioner_manager
 
       # @return [Celluloid::Actor(Bootstrap::Manager)]
       def bootstrap_manager
-        Celluloid::Actor[:bootstrap_manager]
+        Celluloid::Actor[:bootstrap_manager] or raise DeadActorError, "bootstrap manager not running"
       end
       alias_method :bootstrapper, :bootstrap_manager
 
       # @return [Celluloid::Actor(NodeQuerier)]
       def node_querier
-        Celluloid::Actor[:node_querier]
+        Celluloid::Actor[:node_querier] or raise DeadActorError, "node querier not running"
+      end
+
+      def chef_connection
+        config.to_ridley
       end
     end
   end
