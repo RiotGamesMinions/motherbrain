abbb849fb715ca745c5f91ad1a558f6396918837
Justin Campbell <justin@justincampbell.me>
2012-12-12 11:06:07 -0500

    Format, sort, add documentation

--- lib/mb/upgrade/manager.rb
+++ lib/mb/upgrade/manager.rb
@@ -1,9 +1,19 @@
 module MotherBrain
   module Upgrade
+    # @author Justin Campbell <justin@justincampbell.me>
+    #
+    # Manages an upgrade using a {MotherBrain::Upgrade::Worker}.
+    #
     class Manager
       include Celluloid
       include ActorUtil
 
+      # @param [String] environment
+      #
+      # @param [MotherBrain::Plugin] plugin
+      #
+      # @param [Hash] options # TODO: add options documentation
+      #
       def upgrade(environment, plugin, options)
         Worker.new(environment, plugin, options).run
       end
--- lib/mb/upgrade/worker.rb
+++ lib/mb/upgrade/worker.rb
@@ -1,6 +1,12 @@
 module MotherBrain
   module Upgrade
+    # @author Justin Campbell <justin@justincampbell.me>
+    #
+    # Upgrades a plugin by pinning cookbook versions and override attributes
+    # (based on the plugin components' version attributes).
+    #
     class Worker
+      # TODO: use constant from Ridley
       RIDLEY_OPT_KEYS = [
         :server_url,
         :client_name,
@@ -13,14 +19,34 @@ module MotherBrain
         :ssl
       ].freeze
 
-      attr_reader :environment, :plugin, :options
+      # @return [String]
+      attr_reader :environment
 
+      # @return [Hash]
+      attr_reader :options
+
+      # @return [MotherBrain::Plugin]
+      attr_reader :plugin
+
+      # @param [String] environment
+      #
+      # @param [MotherBrain::Plugin] plugin
+      #
+      # @param [Hash] options # TODO: add options documentation
+      #
       def initialize(environment, plugin, options = {})
         @environment = environment
         @plugin = plugin
         @options = options
       end
 
+      # @raise [ComponentNotFound] if a component version is passed that does
+      #   not have a corresponding component in the plugin
+      #
+      # @raise [ComponentNotVersioned] if a component version is passed that
+      #   does not have a version attribute in the corresponding component
+      #
+      # @raise [EnvironmentNotFound] if the environment does not exist
       def run
         assert_environment_exists
 
@@ -33,107 +59,125 @@ module MotherBrain
 
       private
 
-      def component_versions
-        options['component_versions'] || {}
-      end
-
-      def cookbook_versions
-        options['cookbook_versions'] || {}
-      end
-
-      def override_attributes
-        return @override_attributes if @override_attributes
-
-        @override_attributes = {}
-
-        component_versions.each do |component_name, version|
-          @override_attributes[version_attribute(component_name)] = version
+        # @raise [EnvironmentNotFound]
+        def assert_environment_exists
+          unless chef_environment
+            raise EnvironmentNotFound, "Environment '#{environment}' not found"
+          end
         end
 
-        @override_attributes
-      end
-
-      def component(name)
-        result = components.find { |component| component.name == name }
-
-        unless result
-          raise ComponentNotFound,
-            "Component '#{component_name}' not found for plugin '#{plugin.name}'"
+        # @return [Ridley::Connection]
+        def chef_connection
+          @chef_connection = Ridley::Connection.new(options.slice(*RIDLEY_OPT_KEYS))
         end
 
-        result
-      end
-
-      def version_attribute(component_name)
-        result = component(component_name).version_attribute
-
-        unless result
-          raise ComponentNotVersioned.new component_name
+        # @return [Ridley::Environment] # TODO: verify
+        def chef_environment
+          @chef_environment ||= chef_connection.environment.find(environment)
         end
 
-        MB.ui.say "Component '#{component_name}' versioned as '#{result}'"
+        # @param [String] name
+        #
+        # @return [MotherBrain::Component]
+        #
+        # @raise [ComponentNotFound]
+        def component(name)
+          result = components.find { |component| component.name == name }
 
-        result
-      end
+          unless result
+            raise ComponentNotFound,
+              "Component '#{component_name}' not found for plugin '#{plugin.name}'"
+          end
 
-      def components
-        plugin.components
-      end
-
-      def nodes
-        result = plugin.nodes(environment).collect { |group, node| node['default'][0].public_hostname }
-
-        unless result.any?
-          MB.ui.say "No nodes in environment '#{environment}'"
+          result
         end
 
-        result
-      end
-
-      def chef_environment
-        @chef_environment ||= chef_connection.environment.find(environment)
-      end
-
-      def set_component_versions
-        MB.ui.say "Setting cookbook versions #{cookbook_versions}"
-
-        set_override_attributes
-      end
-
-      def set_override_attributes
-        MB.ui.say "Setting override attributes #{override_attributes}"
-
-        chef_environment.override_attributes.merge! override_attributes
-      end
-
-      def set_cookbook_versions
-        MB.ui.say "Setting cookbook versions #{cookbook_versions}"
-
-        chef_environment.cookbook_versions.merge! cookbook_versions
-      end
-
-      def save_environment
-        chef_environment.save
-        @chef_environment = nil
-      end
-
-      def run_chef
-        MB.ui.say "Running chef on #{nodes}"
-
-        nodes.map { |node|
-          Application.node_querier.future.chef_run(node)
-        }.map(&:value)
-      end
-
-      def assert_environment_exists
-        unless chef_environment
-          raise EnvironmentNotFound, "Environment '#{environment}' not found"
+        # @return [Hash]
+        def component_versions
+          options['component_versions'] || {}
         end
-      end
 
-      def chef_connection
-        @chef_connection = Ridley::Connection.new(options.slice(*RIDLEY_OPT_KEYS))
-      end
+        # @return [Hash]
+        def cookbook_versions
+          options['cookbook_versions'] || {}
+        end
+
+        # @return [Array<MotherBrain::Component>]
+        def components
+          plugin.components
+        end
+
+        # @return [Hash]
+        def override_attributes
+          return @override_attributes if @override_attributes
+
+          @override_attributes = {}
+
+          component_versions.each do |component_name, version|
+            @override_attributes[version_attribute(component_name)] = version
+          end
+
+          @override_attributes
+        end
+
+        # @return [Array<String>]
+        def nodes
+          result = plugin.nodes(environment).collect { |group, node| node['default'][0].public_hostname }
+
+          unless result.any?
+            MB.ui.say "No nodes in environment '#{environment}'"
+          end
+
+          result
+        end
+
+        def run_chef
+          MB.ui.say "Running chef on #{nodes}"
+
+          nodes.map { |node|
+            Application.node_querier.future.chef_run(node)
+          }.map(&:value)
+        end
+
+        def save_environment
+          chef_environment.save
+          @chef_environment = nil
+        end
+
+        def set_component_versions
+          MB.ui.say "Setting cookbook versions #{cookbook_versions}"
+
+          set_override_attributes
+        end
+
+        def set_override_attributes
+          MB.ui.say "Setting override attributes #{override_attributes}"
+
+          chef_environment.override_attributes.merge! override_attributes
+        end
+
+        def set_cookbook_versions
+          MB.ui.say "Setting cookbook versions #{cookbook_versions}"
+
+          chef_environment.cookbook_versions.merge! cookbook_versions
+        end
+
+        # @param [String] component_name
+        #
+        # @return [String] the version attribute
+        #
+        # @raise [ComponentNotVersioned]
+        def version_attribute(component_name)
+          result = component(component_name).version_attribute
+
+          unless result
+            raise ComponentNotVersioned.new component_name
+          end
+
+          MB.ui.say "Component '#{component_name}' versioned as '#{result}'"
+
+          result
+        end
     end
   end
 end
