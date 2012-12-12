cd21382c82ad0a8e280b5166f2685c1e1b1dda31
Jamie Winsor <jamie@vialstudios.com>
2012-12-11 21:24:40 -0800

    add log info for rest gateway start

--- lib/mb/rest/gateway.rb
+++ lib/mb/rest/gateway.rb
@@ -13,16 +13,21 @@ module MotherBrain
         workers: 10
       }.freeze
 
+      # @return [Hash]
+      attr_reader :options
+
       # @option options [String] :host ('0.0.0.0')
       # @option options [Integer] :port (1984)
       # @option options [Boolean] :quiet (false)
       # @option options [Integer] :workers (10)
       def initialize(options = {})
-        options       = DEFAULT_OPTIONS.merge(options)
-        options[:app] = REST::API.new
+        @options       = DEFAULT_OPTIONS.merge(options)
+        @options[:app] = REST::API.new
         
-        @pool = ::Reel::RackWorker.pool_link(size: options[:workers], args: [::Rack::Handler::Reel.new(options)])
-        super(options[:host], options[:port], &method(:on_connect))
+        @pool = ::Reel::RackWorker.pool_link(size: @options[:workers], args: [::Rack::Handler::Reel.new(@options)])
+
+        MB.log.info "MotherBrain REST Gatway: Listening on #{@options[:host]}:#{@options[:port]}"
+        super(@options[:host], @options[:port], &method(:on_connect))
       end
 
       # @param [Reel::Connection] connection
