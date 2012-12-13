32b74516ae059cf66136ee0388e24b4583681016
Jamie Winsor <jamie@vialstudios.com>
2012-12-13 06:28:04 -0800

    add --log flag and --debug flag to mbsrv

--- lib/mb/srv_ctl.rb
+++ lib/mb/srv_ctl.rb
@@ -11,16 +11,30 @@ module MotherBrain
       def parse(args, filename)
         options = {
           config: MB::Config.default_path,
-          log_level: Logger::WARN
+          log_level: Logger::WARN,
+          pid: nil
         }
 
         OptionParser.new("Usage: #{filename} [options]") do |opts|
-          opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+          opts.on("-v", "--[no-]verbose", "run with verbose output") do
             options[:log_level] = Logger::INFO
           end
 
-          opts.on("-d", "--daemonize", "Run in daemon mode") do |v|
-            options[:daemonize] = true
+          opts.on("--debug", "run with debug output") do
+            options[:log_level] = Logger::DEBUG
+          end
+
+          opts.on("-d", "--daemonize", "run in daemon mode") do
+            options[:daemonize]    = true
+            options[:log_location] = FileSystem.logs.join('mbsrv.log').to_s
+          end
+
+          opts.on("--pid [PATH]", String, "pid file to read/write from") do |opt|
+            options[:pid] = File.expand_path(opt)
+          end
+
+          opts.on("-l", "--log [PATH}", String, "path to log file") do |opt|
+            options[:log_location] = opt
           end
 
           opts.on_tail("-h", "--help", "Show this message") do
@@ -43,9 +57,14 @@ module MotherBrain
     attr_reader :options
 
     def initialize(options = {})
-      MB::Logging.setup(level: options[:log_level])
       @options = options
-      @config  = MB::Config.from_file(@options[:config])
+
+      if @options[:log_location]
+        setup_logdir
+      end
+
+      MB::Logging.setup(@options.slice(:level, :location))
+      @config = MB::Config.from_file(@options[:config])
 
       unless @options[:daemonize].nil?
         @config.server.daemonize = @options[:daemonize]
@@ -71,13 +90,17 @@ module MotherBrain
       attr_reader :config
 
       def daemonize
-        unless File.writable?(options[:pid])
+        unless File.writable?(File.dirname(options[:pid]))
           puts "startup failed: couldn't write pid to #{options[:pid]}"
           exit 1
         end
 
         Process.daemon
         File.open(options[:pid], 'w+') { f.write Process.pid }
+      end
+
+      def setup_logdir
+        FileUtils.mkdir_p(File.dirname(options[:log_location]))
       end
   end
 end
