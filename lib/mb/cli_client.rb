cee1e662e6e91bb24e433490c92c9edb5b8dde23
Justin Campbell <justin@justincampbell.me>
2012-12-20 15:27:04 -0500

    Create constants, coding standards, pending spec

--- lib/mb/cli_client.rb
+++ lib/mb/cli_client.rb
@@ -3,13 +3,11 @@ module MotherBrain
     attr_reader :jobs
 
     COLUMNS = `tput cols`.to_i
+    SPACE = " "
+    TICK = 0.1
 
     def initialize(*jobs)
       @jobs = jobs
-    end
-
-    def debugging?
-      MB.log.info?
     end
 
     def display
@@ -26,46 +24,52 @@ module MotherBrain
       end
     end
 
-    def clear_line
-      printf "\r#{' ' * COLUMNS}"
-    end
+    private
 
-    def final_status(job)
-      clear_line
+      def clear_line
+        printf "\r#{SPACE * COLUMNS}"
+      end
 
-      puts "\r  [#{job.type}] #{job.state.to_s.capitalize}"
-    end
+      def debugging?
+        MB.log.info?
+      end
 
-    def status(job)
-      clear_line
+      def final_status(job)
+        clear_line
 
-      printf "\r%s [#{job.type}] #{job.status}", spinner.next
+        puts "\r#{SPACE * spinner.next.length} [#{job.type}] #{job.state.to_s.capitalize}"
+      end
 
-      sleep 0.1
-    end
+      def spinner
+        @spinner ||= Enumerator.new do |enumerator|
+          characters = [
+            %w[| / - \\],
+            ["MB", "MB", "MB", "MB", "  "],
+            ["MB", "  "],
+            %w[` ' - . , . - ']
+          # ].sample
+          ].last
 
-    def spinner
-      @spinner ||= Enumerator.new do |enumerator|
-        characters = [
-          %w[| / - \\],
-          ["MB", "MB", "MB", "MB", "  "],
-          ["MB", "  "],
-          %w[` ' - . , . - ']
-        # ].sample
-        ].last
-
-        loop do
-          characters.each do |character|
-            enumerator.yield character
+          loop do
+            characters.each do |character|
+              enumerator.yield character
+            end
           end
         end
       end
-    end
 
-    def wait
-      sleep 0.1 until jobs.all?(&:completed?)
+      def status(job)
+        clear_line
 
-      true
-    end
+        printf "\r%s [#{job.type}] #{job.status}", spinner.next
+
+        sleep TICK
+      end
+
+      def wait
+        sleep TICK until jobs.all?(&:completed?)
+
+        true
+      end
   end
 end
