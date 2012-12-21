002ad11c7aa4b3c601d8cdff966293b03a5da1b0
Justin Campbell <justin@justincampbell.me>
2012-12-21 12:19:55 -0500

    Add documentation

--- lib/mb/cli_client.rb
+++ lib/mb/cli_client.rb
@@ -1,4 +1,13 @@
 module MotherBrain
+  # @author Justin Campbell <justin@justincampbell.me>
+  #
+  # A class for encapsulating view behavior for a user of the CLI.
+  #
+  # @example
+  #
+  #   job = MotherBrain::Job.new
+  #   CliClient.new(job).display
+  #
   class CliClient
     attr_reader :jobs
 
@@ -6,10 +15,13 @@ module MotherBrain
     SPACE = " "
     TICK = 0.1
 
+    # @param [Array<MotherBrain::Job>] jobs
     def initialize(*jobs)
       @jobs = jobs
     end
 
+    # Block and wait for all jobs to be completed, while displaying the status
+    # of each job.
     def display
       wait and return if debugging?
 
@@ -30,16 +42,19 @@ module MotherBrain
         printf "\r#{SPACE * COLUMNS}"
       end
 
+      # @return [Boolean]
       def debugging?
         MB.log.info?
       end
 
+      # @param [MotherBrain::Job] job
       def final_status(job)
         clear_line
 
         puts "\r#{SPACE * spinner.next.length} [#{job.type}] #{job.state.to_s.capitalize}"
       end
 
+      # @return [Enumerator]
       def spinner
         @spinner ||= Enumerator.new do |enumerator|
           characters = [
@@ -58,6 +73,7 @@ module MotherBrain
         end
       end
 
+      # @param [MotherBrain::Job] job
       def status(job)
         clear_line
 
@@ -66,6 +82,7 @@ module MotherBrain
         sleep TICK
       end
 
+      # @return [true]
       def wait
         sleep TICK until jobs.all?(&:completed?)
 
