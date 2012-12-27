3e96351f3dc124efaea8d79a03bc904c72010f6e
Jamie Winsor <jamie@vialstudios.com>
2012-12-26 16:53:52 -0800

    abort on provision failure in plugin_invoker

--- lib/mb/plugin_invoker.rb
+++ lib/mb/plugin_invoker.rb
@@ -69,16 +69,26 @@ module MotherBrain
 
               CliClient.new(job).display
 
-              return if options[:skip_bootstrap]
+              if job.success?
+                MB.ui.say "Provision finished"
 
-              bootstrap_manifest = MB::Bootstrap::Manifest.from_provisioner(
-                job.result,
-                manifest,
-                Tempfile.new('bootstrap_manifest').path
-              )
-              bootstrap_manifest.save
+                if options[:skip_bootstrap]
+                  MB.ui.say "Skipping bootstrap"
+                  MB.ui.say job.result
+                  exit 0
+                end
 
-              invoke(:bootstrap, [environment, bootstrap_manifest.path], options)
+                bootstrap_manifest = MB::Bootstrap::Manifest.from_provisioner(
+                  job.result,
+                  manifest,
+                  Tempfile.new('bootstrap_manifest').path
+                )
+                bootstrap_manifest.save
+
+                invoke(:bootstrap, [environment, bootstrap_manifest.path], options)
+              else
+                exit 1
+              end
             end
 
             method_option :component_versions,
