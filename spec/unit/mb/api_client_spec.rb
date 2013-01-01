71c15e96820a5a07636b4ff757e41d20e6f379f9
Jamie Winsor <jamie@vialstudios.com>
2013-01-01 13:14:49 -0800

    refactor api client resource specific tests into their own spec files

--- spec/unit/mb/api_client_spec.rb
+++ spec/unit/mb/api_client_spec.rb
@@ -26,64 +26,14 @@ describe MB::ApiClient do
   subject { described_class.new }
 
   describe "#config" do
-    before(:each) do
-      stub_request(:get, "http://0.0.0.0:1984/config.json").
-        to_return(status: 200, body: MB::Application.config.to_json)
-    end
-
     it "returns an instance of MB::ApiClient::ConfigResource" do
       subject.config.should be_a(MB::ApiClient::ConfigResource)
-    end
-
-    describe "#show" do
-      it "returns an instance of MB::Config" do
-        subject.config.show.should be_a(MB::Config)
-      end
-
-      it "allows future values" do
-        future = subject.config.future.show
-        future.should be_a(Celluloid::Future)
-        future.value.should be_a(MB::Config)
-      end
     end
   end
 
   describe "#plugin" do
     it "returns an instance of MB::ApiClient::PluginResource" do
       subject.plugin.should be_a(MB::ApiClient::PluginResource)
-    end
-
-    describe "#find" do
-      let(:plugin_id) { "rspec-test" }
-
-      it "returns decoded JSON from /plugins/{plugin_id}.json" do
-        stub_request(:get, "http://0.0.0.0:1984/plugins/#{plugin_id}.json").
-          to_return(status: 200, body: MultiJson.encode({}))
-
-        subject.plugin.find(plugin_id).should be_a(Hash)
-      end
-
-      context "when given a version" do
-        let(:plugin_version) { "1.0.0" }
-
-        it "returns decoded JSON from /plugins/{plugin_id}/{plugin_version}.json" do
-          stub_request(:get, "http://0.0.0.0:1984/plugins/#{plugin_id}/1_0_0.json").
-            to_return(status: 200, body: MultiJson.encode({}))
-
-          subject.plugin.find(plugin_id, plugin_version).should be_a(Hash)
-        end
-      end
-    end
-
-    describe "#list" do
-      before(:each) do
-        stub_request(:get, "http://0.0.0.0:1984/plugins.json").
-          to_return(status: 200, body: MultiJson.encode(MB::PluginManager.instance.plugins))
-      end
-
-      it "returns an Array" do
-        subject.plugin.list.should be_a(Array)
-      end
     end
   end
 end
