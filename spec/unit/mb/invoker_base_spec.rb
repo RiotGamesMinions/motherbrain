dd896fe02c8adc7dac45cf5fdf75eb373bb252a5
Jamie Winsor <jamie@vialstudios.com>
2013-01-28 20:31:33 -0800

    update invoker base specs to match recent changes
    to how the default location of config is determined

--- spec/unit/mb/invoker_base_spec.rb
+++ spec/unit/mb/invoker_base_spec.rb
@@ -4,50 +4,22 @@ describe MB::InvokerBase do
   describe "ClassMethods" do
     subject { MB::InvokerBase }
 
-    describe "default configuration path" do
-      context "when the value of ENV['MB_CONFIG'] specifies a config file that exists" do
-        before(:each) do
-          set_mb_config_path(mb_config_path)
-          generate_valid_config(ENV['MB_CONFIG'])
-        end
-
-        after(:each) { FileUtils.rm(ENV['MB_CONFIG']) }
-
-        it "reads the configuration file as the Invoker's configuration" do
-          invoker = subject.new([])
-
-          invoker.config.chef.api_url.should_not be_nil
-        end
-      end
-
-      context "when a config.json is not found in the directory contained in ENV['MB_CONFIG']" do
-        before(:each) do
-          FileUtils.rm(ENV['MB_CONFIG'])
-        end
-
-        it "raises a Chozo::Errors::ConfigNotFound error configuration" do
-          lambda {
-            subject.new([])
-          }.should raise_error(Chozo::Errors::ConfigNotFound)
-        end
-      end
-    end
-
     describe "specifying a configuration file" do
+      let(:location) { tmp_path.join('config.json').to_s }
+
       before(:each) do
-        set_mb_config_path(mb_config_path)
-        generate_valid_config(ENV['MB_CONFIG'])
+        generate_valid_config(location)
       end
 
       it "loads the specified config file when the specified file exists" do
-        invoker = subject.new([], config: ENV['MB_CONFIG'])
+        invoker = subject.new([], config: location)
 
         invoker.config.chef.api_url.should_not be_nil
       end
 
       it "raises a ConfigNotFound error when the specified path does not exist" do
         lambda {
-          invoker = subject.new([], config: tmp_path.join("config.json"))
+          invoker = subject.new([], config: tmp_path.join("not_there.json"))
         }.should raise_error(Chozo::Errors::ConfigNotFound)
       end
 
