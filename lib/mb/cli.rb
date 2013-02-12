68ae36ca19a44c37dd4b168e9b442ca3cd95893e
Jamie Winsor <jamie@vialstudios.com>
2013-02-11 19:28:11 -0800

    remove autoload for missing class

--- lib/mb/cli.rb
+++ lib/mb/cli.rb
@@ -2,7 +2,6 @@ module MotherBrain
   # @author Jamie Winsor <reset@riotgames.com>
   module Cli
     autoload :Base, 'mb/cli/base'
-    autoload :SubCommand, 'mb/cli/sub_command'
-    autoload :SubCommandBase, 'mb/cli/sub_command_base'
+    autoload :SubCommand, 'mb/cli/sub_command' 
   end
 end
