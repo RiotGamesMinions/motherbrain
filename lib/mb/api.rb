b83e612d083168b170fb2d052c582c4388effafe
Michael Ivey <michael.ivey@riotgames.com>
2013-03-01 00:29:41 -0800

    undo change in API

--- lib/mb/api.rb
+++ lib/mb/api.rb
@@ -55,7 +55,7 @@ module MotherBrain
 
       desc "list all active jobs"
       get 'active' do
-        JobManager.instance.active_jobs
+        JobManager.instance.active
       end
 
       desc "find and return the Job with the given ID"
