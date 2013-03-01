cde36d5d220ab7bdf527e24eaffbd24ca4182421
Jamie Winsor <jamie@vialstudios.com>
2013-02-28 16:40:23 -0800

    exceptions raised in api client middleware will not crash the connection

--- lib/mb/api_client/connection.rb
+++ lib/mb/api_client/connection.rb
@@ -4,6 +4,15 @@ module MotherBrain
     # @api private
     class Connection < Faraday::Connection
       include Celluloid
+
+      # Override Faraday::Connection#run_request to catch exceptions raised in any
+      # middleware and then re-raise them with Celluloid#abort so we don't crash
+      # the connection.
+      def run_request(*args)
+        super
+      rescue => ex
+        abort(ex)
+      end
     end
   end
 end
