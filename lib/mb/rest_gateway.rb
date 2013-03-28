a749fdf24c062298503c03d097175aa0138cb2b2
Jamie Winsor <jamie@vialstudios.com>
2013-03-27 19:53:24 -0700

    terminate the pool on shutdown of the rest gateway

--- lib/mb/rest_gateway.rb
+++ lib/mb/rest_gateway.rb
@@ -30,6 +30,7 @@ module MotherBrain
 
     finalizer do
       log.info { "REST Gateway stopping..." }
+      pool.terminate if pool && pool.alive?
     end
 
     # @option options [String] :host ('0.0.0.0')
@@ -43,7 +44,7 @@ module MotherBrain
       @handler = ::Rack::Handler::Reel.new(@options)
       @pool = ::Reel::RackWorker.pool(size: @options[:workers], args: [@handler])
 
-      MB.log.info "MotherBrain REST Gatway: Listening on #{@options[:host]}:#{@options[:port]}"
+      log.info "MotherBrain REST Gatway: Listening on #{@options[:host]}:#{@options[:port]}"
       super(@options[:host], @options[:port], &method(:on_connect))
     end
 
