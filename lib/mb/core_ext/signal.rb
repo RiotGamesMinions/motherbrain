f96db0df5824aa5c28a333f7b44531e71c22fa16
Jamie Winsor <jamie@vialstudios.com>
2013-04-04 16:24:24 -0700

    add function documentation to Signal ext module

--- lib/mb/core_ext/signal.rb
+++ lib/mb/core_ext/signal.rb
@@ -1,8 +1,12 @@
 # @author Jamie Winsor <reset@riotgames.com>
 module Signal
   class << self
+    # Check if a given signal is supported on the current platform
+    #
+    # @param [#to_s] id
+    #   the signal ID
     def supported?(id)
-      list.has_key?(id)
+      list.has_key?(id.to_s)
     end
   end
 end
