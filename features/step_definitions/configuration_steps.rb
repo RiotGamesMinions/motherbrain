edba08e0cebd782149904a4e1f0e257d81f0a503
Jamie Winsor <jamie@vialstudios.com>
2013-04-05 16:24:03 -0700

    collect all configuration steps into configuration_steps.rb

--- features/step_definitions/configuration_steps.rb
+++ features/step_definitions/configuration_steps.rb
@@ -9,3 +9,10 @@ end
 Given /^an invalid MotherBrain configuration$/ do
   generate_invalid_config(mb_config_path)
 end
+
+Then /^a MotherBrain config file should exist and contain:$/ do |table|
+  config = MB::Config.from_file(mb_config_path)
+  table.raw.each do |key, value|
+    config.get_attribute(key).should eql(value)
+  end
+end
