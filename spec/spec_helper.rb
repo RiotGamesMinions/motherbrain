be6da81c855e9f3da4ca7421c6eb39bc872f560c
Justin Campbell <justin@justincampbell.me>
2013-04-08 10:45:03 -0700

    Randomize specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ def setup_rspec
     config.treat_symbols_as_metadata_keys_with_true_values = true
     config.filter_run focus: true
     config.run_all_when_everything_filtered = true
+    config.order = :random
 
     config.before(:all) do
       MB::Logging.setup(location: '/dev/null')
