f7b937a0e86c4183aedf8520eb215fe230205c0a
Michael Ivey <michael.ivey@riotgames.com>
2013-05-07 08:50:37 -0700

    Convert spec to expect/to syntax

--- spec/unit/mb/provisioners/aws_spec.rb
+++ spec/unit/mb/provisioners/aws_spec.rb
@@ -95,7 +95,7 @@ describe MB::Provisioners::AWS do
       subject.should_receive(:verify_instances).and_return(true)
       subject.should_receive(:verify_connection).and_return(true)
       subject.should_receive(:instances_as_manifest).and_return(response)
-      subject.up(job, env_name, manifest, plugin, skip_bootstrap: true).should eq(response)
+      expect(subject.up(job, env_name, manifest, plugin, skip_bootstrap: true)).to eq(response)
     end
   end
 
@@ -125,15 +125,15 @@ describe MB::Provisioners::AWS do
         end
 
         it "should error on access_key" do
-          lambda { subject.access_key(manifest) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.access_key(manifest) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         it "should error on secret_key" do
-          lambda { subject.secret_key(manifest) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.secret_key(manifest) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         it "should error on endpoint" do
-          lambda { subject.secret_key(manifest) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.secret_key(manifest) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         context "with Euca environment variables" do
@@ -144,9 +144,9 @@ describe MB::Provisioners::AWS do
           end
 
           it "should get from the Euca environment variables" do
-            subject.access_key(manifest).should eq('EC2ABCDEFG')
-            subject.secret_key(manifest).should eq('EC2abcdefgh123456789')
-            subject.endpoint(manifest).should eq('http://euca2.example.com/services/Eucalyptus')
+            expect(subject.access_key(manifest)).to eq('EC2ABCDEFG')
+            expect(subject.secret_key(manifest)).to eq('EC2abcdefgh123456789')
+            expect(subject.endpoint(manifest)).to eq('http://euca2.example.com/services/Eucalyptus')
           end
         end
 
@@ -158,18 +158,18 @@ describe MB::Provisioners::AWS do
           end
 
           it "should get from the AWS environment variables" do
-            subject.access_key(manifest).should eq('AWSABCDEFG')
-            subject.secret_key(manifest).should eq('AWSabcdefgh123456789')
-            subject.endpoint(manifest).should eq('http://ec2.ap-southeast-1.amazonaws.com')
+            expect(subject.access_key(manifest)).to eq('AWSABCDEFG')
+            expect(subject.secret_key(manifest)).to eq('AWSabcdefgh123456789')
+            expect(subject.endpoint(manifest)).to eq('http://ec2.ap-southeast-1.amazonaws.com')
           end
         end
       end
 
       context "with manifest keys" do
         it "should get from the manifest options" do
-          subject.access_key(manifest).should eq('ABCDEFG')
-          subject.secret_key(manifest).should eq('abcdefgh123456789')
-          subject.endpoint(manifest).should eq('http://euca.example.com/services/Eucalyptus')
+          expect(subject.access_key(manifest)).to eq('ABCDEFG')
+          expect(subject.secret_key(manifest)).to eq('abcdefgh123456789')
+          expect(subject.endpoint(manifest)).to eq('http://euca.example.com/services/Eucalyptus')
         end
       end
     end
@@ -177,54 +177,54 @@ describe MB::Provisioners::AWS do
     describe "#validate_manifest_options(job,manifest)" do
       context "with a valid options hash in the manifest" do
         it "returns true" do
-          subject.validate_manifest_options(job,manifest).should eq(true)
+          expect(subject.validate_manifest_options(job,manifest)).to eq(true)
         end
 
         it "does not raise when SecurityGroups is not set" do
           manifest[:options].delete :security_groups
-          lambda { subject.validate_manifest_options(job,manifest).should eq(true) }.should_not raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.validate_manifest_options(job,manifest).should eq(true) }).not_to raise_error(MB::InvalidProvisionManifest)
         end
       end
 
       context "with an invalid options hash in the manifest" do
         it "raises on no options" do
           manifest.delete :options
-          lambda { subject.validate_manifest_options(job,manifest).should eq(true) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.validate_manifest_options(job,manifest).should eq(true) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         it "raises on no ImageId" do
           manifest[:options].delete :image_id
-          lambda { subject.validate_manifest_options(job,manifest).should eq(true) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.validate_manifest_options(job,manifest).should eq(true) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         it "raises on no KeyName" do
           manifest[:options].delete :key_name
-          lambda { subject.validate_manifest_options(job,manifest).should eq(true) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.validate_manifest_options(job,manifest).should eq(true) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         it "raises on no AvailabilityZone" do
           manifest[:options].delete :availability_zone
-          lambda { subject.validate_manifest_options(job,manifest).should eq(true) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.validate_manifest_options(job,manifest).should eq(true) }).to raise_error(MB::InvalidProvisionManifest)
         end
 
         it "raises on SecurityGroups not being an array" do
           manifest[:options][:security_groups] = :fleeble
-          lambda { subject.validate_manifest_options(job,manifest).should eq(true) }.should raise_error(MB::InvalidProvisionManifest)
+          expect(lambda { subject.validate_manifest_options(job,manifest).should eq(true) }).to raise_error(MB::InvalidProvisionManifest)
         end
       end
     end
 
     describe "#instance_counts" do
       it "returns a Hash" do
-        subject.instance_counts(manifest).should be_a(Hash)
+        expect(subject.instance_counts(manifest)).to be_a(Hash)
       end
 
       it "counts the m1.large instances" do
-        subject.instance_counts(manifest)['m1.large'].should eq(6)
+        expect(subject.instance_counts(manifest)['m1.large']).to eq(6)
       end
 
       it "counts the m1.small instances" do
-        subject.instance_counts(manifest)['m1.small'].should eq(2)
+        expect(subject.instance_counts(manifest)['m1.small']).to eq(2)
       end
     end
 
@@ -238,10 +238,10 @@ describe MB::Provisioners::AWS do
     describe "#run_instances" do
       it "keeps track of the instances" do
         result  = subject.run_instances job, fog, {}, "m1.large", 3, manifest.options
-        result.should be_a(Hash)
-        result.should have(3).instances
-        result.each {|k,i| i[:type].should eq("m1.large") }
-        result.each {|k,i| i[:ipaddress].should be_nil }
+        expect(result).to be_a(Hash)
+        expect(result).to have(3).instances
+        result.each {|k,i| expect(i[:type]).to eq("m1.large") }
+        result.each {|k,i| expect(i[:ipaddress]).to be_nil }
       end
     end
 
@@ -276,11 +276,11 @@ describe MB::Provisioners::AWS do
       end
 
       it "returns an array" do
-        subject.instances_as_manifest(instances).should be_an(Array)
+        expect(subject.instances_as_manifest(instances)).to be_an(Array)
       end
 
       it "has 8 instances" do
-        subject.instances_as_manifest(instances).should have(8).instances
+        expect(subject.instances_as_manifest(instances)).to have(8).instances
       end
     end
   end
@@ -292,13 +292,13 @@ describe MB::Provisioners::AWS do
       end
 
       it "should find 3 instances" do
-        subject.instance_ids(env_name).should have(3).instances
+        expect(subject.instance_ids(env_name)).to have(3).instances
       end
 
       it "should find all 3" do
         instance_ids = subject.instance_ids(env_name)
         ["i-ABCDEFG1", "i-ABCDEFG2", "i-ABCDEFG3"].each do |i|
-          instance_ids.should include(i)
+          expect(instance_ids).to include(i)
         end
       end
     end
@@ -309,13 +309,13 @@ describe MB::Provisioners::AWS do
       end
 
       it "should find 3 instances" do
-        subject.instance_ids(env_name).should have(3).instances
+        expect(subject.instance_ids(env_name)).to have(3).instances
       end
 
       it "should find all 3" do
         instance_ids = subject.instance_ids(env_name)
         ["i-EBCDEFG1", "i-EBCDEFG2", "i-EBCDEFG3"].each do |i|
-          instance_ids.should include(i)
+          expect(instance_ids).to include(i)
         end
       end
     end
