c4e8c63be9aa640582a2533036330be7eeee6d97
Justin Campbell <justin@justincampbell.me>
2013-05-23 19:45:35 -0400

    Fix error variable name

--- lib/mb/gears.rb
+++ lib/mb/gears.rb
@@ -2,7 +2,7 @@ Dir["#{File.dirname(__FILE__)}/gears/*.rb"].sort.each do |path|
   basename = File.basename(path, '.rb')
   begin
     require "mb/gears/#{basename}"
-  rescue LoadError => ex
+  rescue LoadError => error
     MB.log.warn "Error loading #{basename} gear: #{error.message}"
   end
 end
