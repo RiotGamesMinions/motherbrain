ad7bc50e14316745d15fe5d4013d0191e7a49338
Jamie Winsor <jamie@vialstudios.com>
2013-05-24 01:03:56 -0700

    move MB::Cli::Base.shell to MB::Cli::Shell.shell

--- lib/mb/cli/base.rb
+++ lib/mb/cli/base.rb
@@ -14,28 +14,9 @@ module MotherBrain
           self.register(klass, klass.name, klass.usage, klass.description)
         end
 
-        # Returns the shell used in the motherbrain CLI. If you are in a Unix platform
-        # it will use a colored shell, otherwise it will use a color-less one.
-        def shell
-          @shell ||= if ENV['MB_SHELL'] && ENV['MB_SHELL'].size > 0
-            MB::Cli::Shell.const_get(ENV['MB_SHELL'].capitalize)
-          elsif Chozo::Platform.windows? && !ENV['ANSICON']
-            MB::Cli::Shell::Basic
-          else
-            MB::Cli::Shell::Color
-          end
-        end
-
-        # Set the shell used in the motherbrain CLI
-        #
-        # @param [Constant] klass
-        def shell=(klass)
-          @shell = klass
-        end
-
         # @return [MB::Cli::Shell::Color, MB::Cli::Shell::Basic]
         def ui
-          @ui ||= shell.new
+          @ui ||= MB::Cli::Shell.shell.new
         end
       end
 
@@ -54,7 +35,5 @@ module MotherBrain
         end
       end
     end
-
-    Thor::Base.shell = MB::Cli::Base.shell
   end
 end
--- lib/mb/cli/shell.rb
+++ lib/mb/cli/shell.rb
@@ -5,6 +5,26 @@ module MotherBrain
       autoload :Basic, 'mb/cli/shell/basic'
       autoload :Color, 'mb/cli/shell/color'
       autoload :Ext, 'mb/cli/shell/ext'
+
+      class << self
+        attr_writer :shell
+
+        # Returns the shell used in the motherbrain CLI. If you are in a Unix platform
+        # it will use a colored shell, otherwise it will use a color-less one.
+        #
+        # @return [Shell::Basic, Shell::Color]
+        def shell
+          @shell ||= if ENV['MB_SHELL'] && ENV['MB_SHELL'].size > 0
+            Shell.const_get(ENV['MB_SHELL'].capitalize)
+          elsif Chozo::Platform.windows? && !ENV['ANSICON']
+            Shell::Basic
+          else
+            Shell::Color
+          end
+        end
+      end
+
+      ::Thor::Base.shell = shell
     end
   end
 end
