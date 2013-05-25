519f54f9e0a7ed8b06c659da3232303a8f8e04a3
Michael Ivey <michael.ivey@riotgames.com>
2013-05-24 18:54:19 -0700

    docs

--- lib/mb/bootstrap/template.rb
+++ lib/mb/bootstrap/template.rb
@@ -1,9 +1,16 @@
 module MotherBrain
   module Bootstrap
     # @author Michael Ivey <michael.ivey@riotgames.com>
-    #
     class Template
       class << self
+        # Install a bootstrap template into the templates directory
+        #
+        # @param [String] name
+        #   the name of the template
+        # @param [String] filename_or_url
+        #   a local filename or URL to download
+        #
+        # @raise [MB::BootstrapTemplateNotFound] if the file cannot be installed
         def install(name, filename_or_url)
           MB.log.info "Installing bootstrap template `#{name}` from #{filename_or_url}"
           name += ".erb"
@@ -27,6 +34,15 @@ module MotherBrain
           end
         end
 
+        # Find and validate a user-chosen template
+        #
+        # @param [String] name_or_path
+        #  User's choice of template
+        #
+        # @return [String] the actual path the the chosen template
+        #
+        # @raise [MB::BootstrapTemplateNotFound] if the template file
+        #   does not exist
         def find(name_or_path=nil)
           name_or_path ||= MB::Application.config.bootstrap.default_template
           installed = MB::FileSystem.templates.join("#{name_or_path}.erb").to_s
