edd3450ee7ef13ef32f6dffdd75c605ea330ded9
Justin Campbell <justin@justincampbell.me>
2013-06-04 14:42:55 -0400

    Refactor MB::FileSystem specs

--- spec/unit/mb/file_system_spec.rb
+++ spec/unit/mb/file_system_spec.rb
@@ -1,47 +1,69 @@
 require 'spec_helper'
 
 describe MB::FileSystem do
-  subject { described_class }
+  describe ".init" do
+    subject(:init) { described_class.init }
 
-  describe "::logs" do
-    it "returns a pathname" do
-      subject.logs.should be_a(Pathname)
+    it "creates the directory structure" do
+      FileUtils.should_receive(:mkdir_p).exactly(5).times
+
+      init
     end
+  end
+
+  describe ".logs" do
+    subject(:logs) { described_class.logs }
+
+    it { should be_a(Pathname) }
 
     it "is a path within the root of the motherbrain filesystem" do
-      subject.logs.to_s.should include(MB::FileSystem.root.to_s)
+      expect(logs.to_s).to include(MB::FileSystem.root.to_s)
     end
   end
 
-  describe "::manifests" do
-    it "returns a pathname" do
-      subject.manifests.should be_a(Pathname)
-    end
-  end
+  describe ".manifests" do
+    subject(:manifests) { described_class.manifests }
 
-  describe "::root" do
-    it "returns a pathname" do
-      subject.root.should be_a(Pathname)
-    end
-  end
-
-  describe "::tmp" do
-    it "returns a pathname" do
-      subject.tmp.should be_a(Pathname)
-    end
+    it { should be_a(Pathname) }
 
     it "is a path within the root of the motherbrain filesystem" do
-      subject.tmp.to_s.should include(MB::FileSystem.root.to_s)
+      expect(manifests.to_s).to include(MB::FileSystem.root.to_s)
     end
   end
 
-  describe "::tmpdir" do
-    it "returns a string" do
-      subject.tmpdir.should be_a(String)
+  describe ".root" do
+    subject(:root) { described_class.root }
+
+    it { should be_a(Pathname) }
+  end
+
+  describe ".tmp" do
+    subject(:tmp) { described_class.tmp }
+
+    it { should be_a(Pathname) }
+
+    it "is a path within the root of the motherbrain filesystem" do
+      expect(tmp.to_s).to include(MB::FileSystem.root.to_s)
     end
+  end
+
+  describe ".tmpdir" do
+    subject(:tmpdir) { described_class.tmpdir }
+
+    it { should be_a(String) }
 
     it "creates a temporary directory in the motherbrain temp filesystem" do
-      subject.tmpdir.should include(MB::FileSystem.tmp.to_s)
+      expect(tmpdir).to include(MB::FileSystem.tmp.to_s)
+    end
+
+    context "when given a prefix" do
+      subject(:tmpdir) { described_class.tmpdir(prefix) }
+
+      let(:prefix) { "test" }
+
+      it "creates a directory with that prefix" do
+        expect(tmpdir.split(?/).last).to be_start_with(prefix)
+      end
     end
   end
 end
