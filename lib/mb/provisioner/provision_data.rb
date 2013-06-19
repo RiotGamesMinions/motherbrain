d0017311952e551439d79ce2c8190d1510a2d963
Justin Campbell <justin@justincampbell.me>
2013-06-19 15:39:30 -0400

    Add documentation

--- lib/mb/provisioner/provision_data.rb
+++ lib/mb/provisioner/provision_data.rb
@@ -13,30 +13,45 @@ module MotherBrain
         @environment_name = environment_name
       end
 
+      # Returns a array of stored instance data
+      #
+      # @return [Array(Hash)]
       def instances
         attributes[:instances] ||= []
 
         attributes[:instances]
       end
 
+      # Adds instances to the provision data idempotently
+      #
+      # @param [Array(Hash)] instance_array
       def add_instances(instance_array)
         instance_array.each do |instance|
           instances.push instance unless instances.include?(instance)
         end
       end
 
+      # Removes an instance from instances by matching a key/value pair
+      #
+      # @param [Symbol] key
+      # @param [Object] value
       def remove_instance(key, value)
         instances.delete_if { |instance| instance[key] == value }
       end
 
+      # @return [String, nil]
       def provisioner_name
         attributes[:provisioner_name]
       end
 
+      # Sets the provisioner name
+      #
+      # @param [Symbol] value
       def provisioner_name=(value)
         attributes[:provisioner_name] = value
       end
 
+      # Persists the data to the Chef server
       def save
         data_bag_item.save
       end
