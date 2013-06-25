025af0810b9a5ed7057c3ecbfb401bd70d12eab4
Jamie Winsor <jamie@vialstudios.com>
2013-06-24 21:58:48 -0700

    remove explicit setting of json in MB::API::Endpoint
    it doesn't get inherited and will just confuse people

--- lib/mb/api/endpoint.rb
+++ lib/mb/api/endpoint.rb
@@ -2,8 +2,6 @@ require 'grape'
 
 module MotherBrain::API
   class Endpoint < Grape::API
-    format :json
-
     # Force inbound requests to be JSON
     def call(env)
       env['CONTENT_TYPE'] = 'application/json'
