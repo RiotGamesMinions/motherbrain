1306ae16587747d3eb11d5c32c45cf40accbe312
Jamie Winsor <jamie@vialstudios.com>
2013-07-05 23:55:16 -0700

    remove unnecessary check in ActorMocking support

--- spec/support/actor_mocking.rb
+++ spec/support/actor_mocking.rb
@@ -1,10 +1,7 @@
 RSpec.configuration.before(:each) do
   class Celluloid::ActorProxy
-    unless @rspec_compatible
-      @rspec_compatible = true
-      [ :should_receive, :should_not_receive, :stub, :stub_chain, :should, :should_not ].each do |method|
-        undef_method(method) if method_defined?(method)
-      end
+    [ :should_receive, :should_not_receive, :stub, :stub_chain, :should, :should_not ].each do |method|
+      undef_method(method) if method_defined?(method)
     end
   end
 end
