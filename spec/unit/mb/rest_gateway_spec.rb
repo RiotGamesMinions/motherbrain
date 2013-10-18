3fbbeba3ec9c0b23dd91cc0cae5939d51c292aed
Josiah Kiehl <jkiehl@riotgames.com>
2013-10-18 10:40:06 -0700

    RestGateway should subclass Reel::Server

--- spec/unit/mb/rest_gateway_spec.rb
+++ spec/unit/mb/rest_gateway_spec.rb
@@ -3,13 +3,9 @@ require 'spec_helper'
 describe MB::RestGateway do
   include Rack::Test::Methods
 
-  before(:all) { MB::RestGateway.start(port: 26101) }
-  after(:all) { MB::RestGateway.stop }
-  let(:app) { MB::RestGateway.instance.rack_app }
-
   describe "#rack_app" do
     it "returns MB::API::Application" do
-      subject.rack_app.should be_a(MB::API::Application)
+      subject.app.should be_a(MB::API::Application)
     end
   end
 end
