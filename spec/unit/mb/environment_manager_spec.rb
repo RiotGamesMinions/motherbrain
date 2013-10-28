4dd49eb014fc25a24dcd7ae57e6ef4399512a907
Kyle Allan <kallan@riotgames.com>
2013-10-28 14:57:08 -0700

    environment_configure should display better logging

--- spec/unit/mb/environment_manager_spec.rb
+++ spec/unit/mb/environment_manager_spec.rb
@@ -39,6 +39,43 @@ describe MB::EnvironmentManager do
         expect(ticket.result).to be_a(MB::EnvironmentNotFound)
       end
     end
+
+    context "when there are nodes in the environment" do
+      let(:job) { double(set_status: nil, report_running: nil, report_success: nil, report_failure: nil, alive?: false) }
+      let(:ticket) { nil }
+      let(:node_one) { double(name: "surfing", public_hostname: "surfing.riotgames.com") }
+      let(:node_two) { double(name: "football", public_hostname: "football.riotgames.com") }
+      let(:response_one) { double(value: double(host: "surfing.riotgames.com")) }
+      let(:response_two) { double(value: double(host: "football.riotgames.com")) }
+
+      let(:nodes) { [node_one, node_two] }
+
+      before do
+        chef_environment(environment_name)
+        environment_manager.stub(:nodes_for_environment).and_return(nodes)
+        nodes.stub(:collect).and_return([response_one, response_two])
+      end
+
+      it "attempts to run chef" do
+        subject.configure(job, environment_name, options)
+        expect(job).to have_received(:report_success).with(
+          "Finished chef client run on 2 node(s) - surfing.riotgames.com, football.riotgames.com")
+      end
+
+      context "when nodes fail" do
+
+        before do
+          response_one.stub(:value).and_raise(MB::RemoteCommandError.new(nil, "surfing.riotgames.com"))
+          response_two.stub(:value).and_raise(MB::RemoteCommandError.new(nil, "football.riotgames.com"))
+        end
+
+        it "describes the unsuccessful nodes" do
+          subject.configure(job, environment_name, options)
+          expect(job).to have_received(:report_failure).with(
+            "chef client run failed on 2 node(s) - surfing.riotgames.com, football.riotgames.com")
+        end
+      end
+    end
   end
 
   describe "#find" do
