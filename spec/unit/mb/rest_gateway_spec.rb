65114ea6270878a7c95cb187c3aa3cbf31e76068
Josiah Kiehl <jkiehl@riotgames.com>
2014-05-20 15:28:50 -0700

    Add test for setting $PORT

--- spec/unit/mb/rest_gateway_spec.rb
+++ spec/unit/mb/rest_gateway_spec.rb
@@ -12,6 +12,13 @@ describe MB::RestGateway do
   end
 
   describe "constants" do
+    it "should set DEFAULT_PORT to $PORT" do
+      p = 12345
+      ENV["PORT"] = p.to_s
+      load File.join(File.dirname(__FILE__), "..", "..", "..", "lib", "mb", "rest_gateway.rb")
+      expect(MB::RestGateway::DEFAULT_PORT).to eq(p)
+    end
+
     it "should set DEFAULT_PORT to 26100 if $PORT is not set" do
       ENV.delete("PORT")
       load File.join(File.dirname(__FILE__), "..", "..", "..", "lib", "mb", "rest_gateway.rb")
